718a02ad40f5271aebf2c1cb4891d806a9a2a043
1400050-UNIVESP <1400050@aluno.univesp.br>
2021-10-24 14:00:59 -0300

    alterando index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8" />
+	<title> Controle de Extintores</title>
+	<link rel="stylesheet" type="text/css" href="_css/estilo.css" />
+</head>
+
+<body>
+<div id="interface">
+		<header id="cabecalho">
+			<hgroup>
+				<h1>Controle de Extintores</h1>
+				<h2>Condomínio Guaraú II</h2>
+			</hgroup>
+			<nav id="menu">
+				<h1>Menu Principal</h1>
+				<ul>
+					<li onmouseover="mudaFoto ()"><a href="extintores.php">Extintores</a></li>
+					<li onmouseover="mudaFoto ()"><a href="vistorias.php">Vistorias</a></li>
+					<li onmouseover="mudaFoto ()"><a href="manutencao.php">Manutenções</a></li>
+
+				</ul>
+				</ul>
+			</nav>
+		</header>
+		<figure class="foto-legenda">
+			<footer id="rodape">
+				<p>Copyright &copy; 2021 - by UNIVESP - Grupo 003 - Projeto Integrador 1 - São Vicente<br />
+			</footer>
+	</div>
+	<script src="_javascript/funcoes.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+
+</html>
