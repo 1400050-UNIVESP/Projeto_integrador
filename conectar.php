af332f3fb382c7e385bfdae71d33b10cabd84edc
1400050-UNIVESP <90873966+1400050-UNIVESP@users.noreply.github.com>
2022-04-04 23:50:04 -0300

    Add files via upload

---
+++ conectar.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "Simulador_vestibular";
+$port = 3306;
+
+$conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
