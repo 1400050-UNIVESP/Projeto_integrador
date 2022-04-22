4f75f9d6edf43296c6f1589de58cf26f572eea6a
1400050-UNIVESP <90873966+1400050-UNIVESP@users.noreply.github.com>
2022-04-22 16:11:23 -0300

    Add files via upload

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -21,3 +21,65 @@ function deletarUsuario() {
 		vistoriaForm.style.display = 'none';
 	}
 }
+function inserirRespostas() {
+	var crudForm = document.getElementById("inserir_respostas_formulario");
+	crudForm.classList.toggle("visible");
+
+	if (crudForm.classList.contains("visible")) {
+		crudForm.style.display = 'inherit';
+	} else {
+		crudForm.style.display = 'none';
+	}
+}
+function alterarRespostas() {
+	var crudForm = document.getElementById("alterar_respostas_formulario");
+	crudForm.classList.toggle("visible");
+
+	if (crudForm.classList.contains("visible")) {
+		crudForm.style.display = 'inherit';
+	} else {
+		crudForm.style.display = 'none';
+	}
+}
+
+function deletarRespostas() {
+	var crudForm = document.getElementById("deletar_respostas_formulario");
+	crudForm.classList.toggle("visible");
+
+	if (crudForm.classList.contains("visible")) {
+		crudForm.style.display = 'inherit';
+	} else {
+		crudForm.style.display = 'none';
+	}
+}
+function inserirPerguntas() {
+	var crudForm = document.getElementById("inserir_perguntas_formulario");
+	crudForm.classList.toggle("visible");
+
+	if (crudForm.classList.contains("visible")) {
+		crudForm.style.display = 'inherit';
+	} else {
+		crudForm.style.display = 'none';
+	}
+}
+function alterarPerguntas() {
+	var crudForm = document.getElementById("alterar_perguntas_formulario");
+	crudForm.classList.toggle("visible");
+
+	if (crudForm.classList.contains("visible")) {
+		crudForm.style.display = 'inherit';
+	} else {
+		crudForm.style.display = 'none';
+	}
+}
+
+function deletarPerguntas() {
+	var crudForm = document.getElementById("deletar_perguntas_formulario");
+	crudForm.classList.toggle("visible");
+
+	if (crudForm.classList.contains("visible")) {
+		crudForm.style.display = 'inherit';
+	} else {
+		crudForm.style.display = 'none';
+	}
+}
